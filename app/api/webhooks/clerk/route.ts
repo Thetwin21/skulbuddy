3aa114f68ffd821e3496881aa231308afbcac98d
onyeka <onyxojei@gmail.com>
2025-02-09 10:40:37 +0100

    commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,10 +2,10 @@ import { clerkClient } from '@clerk/nextjs/server';
 import { WebhookEvent } from '@clerk/nextjs/server';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { Webhook } from 'svix'
+// import { Webhook } from 'svix'
 
 import { createUser } from '@/lib/actions/user.action';
 
 export async function POST(req: Request) {
-    
+
 }
